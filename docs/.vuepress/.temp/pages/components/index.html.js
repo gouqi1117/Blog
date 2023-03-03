449475d027dbca5408aa5bb0509c72703fba4290
姚亦姚 <11168691+gouqiyao@user.noreply.gitee.com>
2023-03-03 14:49:39 +0800

    test3

--- docs/.vuepress/.temp/pages/components/index.html.js
+++ docs/.vuepress/.temp/pages/components/index.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-1020f180\",\"path\":\"/components/\",\"title\":\"sdsdsdsdsd\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1677224061000,\"contributors\":[{\"name\":\"姚亦姚\",\"email\":\"11168691+gouqiyao@user.noreply.gitee.com\",\"commits\":1}]},\"filePathRelative\":\"components/README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"Hello VuePress\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1677226616000,\"contributors\":[{\"name\":\"姚亦姚\",\"email\":\"11168691+gouqiyao@user.noreply.gitee.com\",\"commits\":2}]},\"filePathRelative\":\"README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
